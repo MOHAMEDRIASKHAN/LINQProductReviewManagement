4580c28a3eccc865cfe1a5720dcf61981a3ee389
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-18 08:33:43 -0700

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Product Review Management");
+
+        }
+    }
+}
